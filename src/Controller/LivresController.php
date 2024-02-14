71f3bf47b879b770fae32987cd84c1b913639dfb
chafroud sabri <chafroud.sabri@hotmail.fr>
2024-02-14 08:38:58 +0100

    version2

--- src/Controller/LivresController.php
+++ src/Controller/LivresController.php
@@ -14,85 +14,85 @@ use Symfony\Component\HttpFoundation\Request;
 class LivresController extends AbstractController
 {
 
-//afficher tout les livres
-//-------------------------
-    #[Route('/livres', name: 'app_livres',methods:['GET'])]
-    public function index(LivresRepository $livresRepository): Response
-    {
-        return $this->render('livres/all_livres.html.twig', [
-                'livres'=>$livresRepository->findAll(),
-            ]);
-    }
+    //afficher tout les livres
+    //-------------------------
+        #[Route('/livres', name: 'app_livres',methods:['GET'])]
+        public function index(LivresRepository $livresRepository): Response
+        {
+            return $this->render('livres/all_livres.html.twig', [
+                    'livres'=>$livresRepository->findAll(),
+                ]);
+        }
 
 
-//view livre par ligne
-//------------------------
-    // #[Route('/livres/{id}', name: 'edit_livre',methods:['GET'])]
-    // public function livreEdit(int $id, LivresRepository $livreRepository)
-    // {
-    //     $livre= $livreRepository->find($id);
-    //     // var_dump($livre);
-    //     return $this->render('livres/edit_livre.html.twig',[
-    //         'livre'=>$livre,
-    //     ]);
-    // }
+    //view livre par ligne
+    //------------------------
+        // #[Route('/livres/{id}', name: 'edit_livre',methods:['GET'])]
+        // public function livreEdit(int $id, LivresRepository $livreRepository)
+        // {
+        //     $livre= $livreRepository->find($id);
+        //     // var_dump($livre);
+        //     return $this->render('livres/edit_livre.html.twig',[
+        //         'livre'=>$livre,
+        //     ]);
+        // }
 
     
-//Update livre
-//--------------
-    #[Route('/livres{id}/update', name: 'update_livre',methods:['GET','POST'])]
-    public function edit_livre(int $id, Request $request, LivresRepository $livresRepository, EntityManagerInterface $entityManager): Response
-    {
-      $form= $this-> createForm(LivresType::class, $livresRepository->find($id));
-      $form->handleRequest($request);
-      if ($form->isSubmitted() && $form->isValid()) {
-          $entityManager->flush();
-
-      return $this->redirectToRoute('app_livres',[],
-      Response::HTTP_SEE_OTHER);
-    }
-    return $this->render('livres/update_livre.html.twig', [
-      'form'=> $form, 'livre'=> $livresRepository->findAll(),
-    ]);
-  }
-
-
-//Ajout livre
-//-------------
-#[Route('/livres/ajout', name: 'ajout_livre', methods:['GET', 'POST'])]
-    public function ajout_livre(Request $request, EntityManagerInterface $entityManager,  LivresRepository $livresRepository ): Response
-    {
-        
-        $livre = new Livres();
-        $form = $this->createForm(LivresType::class, $livre);
+    //Update livre
+    //--------------
+        #[Route('/livres{id}/update', name: 'update_livre',methods:['GET','POST'])]
+        public function edit_livre(int $id, Request $request, LivresRepository $livresRepository, EntityManagerInterface $entityManager): Response
+        {
+        $form= $this-> createForm(LivresType::class, $livresRepository->find($id));
         $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
 
-      if ($form->isSubmitted() && $form->isValid())
-            {  
-            $entityManager->persist($livre); // persist fait le lien entre l'ORM et symfony
-            $entityManager->flush();              //flush fait le lien et applique les changements a la base de donnée
-            return $this->redirectToRoute('app_livres', [], Response::HTTP_SEE_OTHER);  // Redirigez l'utilisateur vers une autre page, par exemple la liste des livres
-            }
-
-        return $this->render('livres/ajout_livre.html.twig', [
-            'form' => $form->createView()
+        return $this->redirectToRoute('app_livres',[],
+        Response::HTTP_SEE_OTHER);
+        }
+        return $this->render('livres/update_livre.html.twig', [
+        'form'=> $form, 'livre'=> $livresRepository->findAll(),
         ]);
     }
 
 
+    //Ajout livre
+    //-------------
+    #[Route('/livres/ajout', name: 'ajout_livre', methods:['GET', 'POST'])]
+        public function ajout_livre(Request $request, EntityManagerInterface $entityManager,  LivresRepository $livresRepository ): Response
+        {
+            
+            $livre = new Livres();
+            $form = $this->createForm(LivresType::class, $livre);
+            $form->handleRequest($request);
 
-//delete livre 
-//--------------
-#[Route('/livres/{id}/delete', name: 'delete_livre')]
-    public function delete_livre( int $id, EntityManagerInterface $entityManager,  LivresRepository $livresRepository ): Response
-    {
-        $livre = $livresRepository->find($id);  //récupère le livre à partir de l'Id
-        var_dump($livre);
-        $entityManager->remove($livre);
+        if ($form->isSubmitted() && $form->isValid())
+                {  
+                $entityManager->persist($livre); // persist fait le lien entre l'ORM et symfony
+                $entityManager->flush();              //flush fait le lien et applique les changements a la base de donnée
+                return $this->redirectToRoute('app_livres', [], Response::HTTP_SEE_OTHER);  // Redirigez l'utilisateur vers une autre page, par exemple la liste des livres
+                }
 
-        $entityManager->flush();
-        return $this->redirectToRoute('app_livres');
-    }
+            return $this->render('livres/ajout_livre.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+
+
+
+    //delete livre 
+    //--------------
+    #[Route('/livres/{id}/delete', name: 'delete_livre')]
+        public function delete_livre( int $id, EntityManagerInterface $entityManager,  LivresRepository $livresRepository ): Response
+        {
+            $livre = $livresRepository->find($id);  //récupère le livre à partir de l'Id
+            var_dump($livre);
+            $entityManager->remove($livre);
+
+            $entityManager->flush();
+            return $this->redirectToRoute('app_livres');
+        }
 
 
 
